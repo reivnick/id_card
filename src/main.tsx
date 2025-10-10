a903c87587e5d925be37e53baa927872f4ad0d84
Benediktus Hosni I <37971110+reivnick@users.noreply.github.com>
2025-10-10 13:47:39 +0700

    Update - Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { SpeedInsights } from "@vercel/speed-insights/react"
 import { Analytics } from "@vercel/analytics/react"
 import './App.css'
 import App from './App.tsx'
@@ -17,7 +16,6 @@ if (typeof window !== 'undefined') {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <SpeedInsights />
     <Analytics />
   </StrictMode>,
 )
